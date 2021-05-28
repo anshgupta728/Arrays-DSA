dda95c1c0c431aac1ddd0afea755e1a378bad30f
Ansh Gupta <71687947+anshgupta0511@users.noreply.github.com>
2021-05-29 00:07:53 +0530

    Add files via upload

---
+++ src/Questions/BinarySearch.java
@@ -0,0 +1,26 @@
+package Questions;
+//O(logn)
+public class BinarySearch{
+
+	public static int binarysearch(int arr[],int ele,int low,int high) {
+		if(low>high)
+			return -1;
+		int mid=(low+high)/2;
+		if(arr[mid]==ele)
+			return mid;
+		if(ele>arr[mid])
+			return binarysearch(arr,ele,mid+1,high);
+		else
+			return binarysearch(arr,ele,low,mid-1);
+	}
+	public static void main(String[] args){
+		int arr[]= {12,27,36,45,56,78,89,91};
+		int ele=78;
+		int pos=binarysearch(arr,ele,0,arr.length-1);
+		if(pos==-1)
+			System.out.println("Element not found");
+		else
+			System.out.println("Element found at position "+(pos+1));
+	}
+
+}
