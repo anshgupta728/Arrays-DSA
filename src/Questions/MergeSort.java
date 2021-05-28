edd6ee91cff3bfc9a8f030ea3be40224a9612348
Ansh Gupta <71687947+anshgupta0511@users.noreply.github.com>
2021-05-28 20:56:48 +0530

    Update MergeSort.java

--- src/Questions/MergeSort.java
+++ src/Questions/MergeSort.java
@@ -1,50 +1,53 @@
 package Questions;
 import java.util.Scanner;
-public class MergeSortAlgorithm{
-	public static void sort(int arr[],int temp[],int low,int mid,int high)
-	{
-		int i=low,j=mid+1,k=low;
-		while(i<=mid && j<=high)
-		{
-			if(arr[i]<arr[j])
-				temp[k++]=arr[i++];
-			else
-				temp[k++]=arr[j++];
-		}
-		while(i<=mid)
-			temp[k++]=arr[i++];
-		while(j<=high)
-			temp[k++]=arr[j++];
+public class MergeSort {
+	public static void sort(int arr[],int low,int mid,int high) {
+		int n1=mid-low+1;
+		int n2=high-mid;
+		int L[]=new int[n1];
+		int R[]=new int[n2];
+		for(int i=0;i<n1;i++)
+			L[i]=arr[low+i];
+		for (int i=0;i<n2;i++)
+            R[i]=arr[mid+1+i];
+		int i = 0, j = 0,k=low;
+        while (i<n1&&j<n2) {
+            if (L[i] <= R[j])
+                arr[k++] = L[i++];
+            else 
+                arr[k++] = R[j++];
+        }
+        while (i<n1) 
+        	arr[k++] = L[i++];
+        while (j<n2)
+        	arr[k++] = R[j++];
 	}
 	
-	public static void mergesort(int arr[],int temp[],int low,int high)
-	{
-		if(low<high)
-		{
+	public static void mergesort(int arr[],int low,int high) {
+		if(low<high) {
 			int mid=(low+high)/2;
-			mergesort(arr,temp,low,mid);
-			mergesort(arr,temp,mid+1,high);
-			sort(arr,temp,low,mid,high);
+			mergesort(arr,low,mid);
+			mergesort(arr,mid+1,high);
+			sort(arr,low,mid,high);
 		}
 	}
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter length of array");
 		int n=sc.nextInt();
 		int arr[]=new int[n];
 		for(int i=0;i<n;i++)
 			arr[i]=sc.nextInt();
-		int temp[]=new int[n];
-		mergesort(arr,temp,0,n-1);
-		System.out.println("*******************************************");
 		System.out.println("Original Array");
 		for(int i:arr)
 			System.out.print(i+" ");
 		System.out.println();
+		mergesort(arr,0,n-1);
 		System.out.println("Sorted Array");
-		for(int i:temp)
+		for(int i:arr)
 			System.out.print(i+" ");
 		sc.close();
 	}
 }
+
+	
