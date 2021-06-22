9db011cad6285303d4330285764e154d472ab65c
Ansh Gupta <71687947+anshgupta0511@users.noreply.github.com>
2021-06-22 21:19:10 +0530

    Add files via upload

--- src/Questions/BubbleSort.java
+++ src/Questions/BubbleSort.java
@@ -7,6 +7,8 @@ public class BubbleSort {
 		int arr[]=new int[n];
 		for(int i=0;i<n;i++)
 			arr[i]=sc.nextInt();
+		
+		//Normal Bubble Sort -> O(n*n) for all cases
 		for(int i=0;i<n-1;i++) {
 			for(int j=0;j<n-i-1;j++) {
 				if(arr[j+1]<arr[j]) {
@@ -16,8 +18,25 @@ public class BubbleSort {
 				}
 			}
 		}
+		
+		//Improvised Bubble Sort -> O(n*n) for worst and average and O(n) for best case
+		/*boolean swapped=false;
+		for(int i=0;i<n-1;i++) {
+			swapped=false;
+			for(int j=0;j<n-i-1;j++) {
+				if(arr[j+1]<arr[j]) {
+					int temp=arr[j+1];
+					arr[j+1]=arr[j];
+					arr[j]=temp;
+					swapped=true;
+				}
+				if(!swapped)
+					break;
+			}
+		}*/
+		
 		for(int i=0;i<n;i++)
-			System.out.print(arr[i]+"\t");
+			System.out.print(arr[i]+" ");
 		sc.close();
 	}
 }
